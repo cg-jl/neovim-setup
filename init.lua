602259e0cabf8f0ed94a347e0f3c94728f33a0f4
Jesús (gsus) Lapastora <cyber.gsuscode@gmail.com>
2023-11-13 13:58:37 +0100

    feat: Treesitter textobjects

--- init.lua
+++ init.lua
@@ -114,6 +114,7 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 		config = function()
 			local ts = require("nvim-treesitter.configs")
 			ts.setup({
@@ -125,12 +126,37 @@ require("lazy").setup({
 						init_selection = "-",
 						node_incremental = "-",
 						scope_incremental = false,
-						node_decremental = "\\",
+						node_decremental = "<bs>",
 					},
 				},
 			})
 		end,
 	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		lazy = true,
+
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forwards to textobj
+						lookahead = true,
+						keymaps = {
+							-- va=, etc all have their v/c commands
+							["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+							["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+							["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+							["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+						},
+					},
+				},
+			})
+		end,
+	},
+
 	{ "nvim-treesitter/nvim-treesitter-context", dependencies = "nvim-treesitter/nvim-treesitter" },
 	"tpope/vim-commentary",
 	"tpope/vim-repeat",
