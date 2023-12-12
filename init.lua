b38f96e1f248501b8a3a2be56379d2e8bcccab50
Jesús "gsus" Lapastora <cyber.gsuscode@gmail.com>
2023-12-12 12:17:14 +0100

    tree-sitter: Add Jakt parser

--- init.lua
+++ init.lua
@@ -169,6 +169,17 @@ require("lazy").setup({
 					},
 				},
 			})
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.jakt = {
+				install_info = {
+					url = "https://github.com/SerenityOS/tree-sitter-jakt.git", -- local path or git repo
+					files = { "src/parser.c", "src/scanner.c" },
+					-- optional entries:
+					branch = "main", -- default branch in case of git repo if different from master
+					generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+					requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+				},
+			}
 		end,
 	},
 
